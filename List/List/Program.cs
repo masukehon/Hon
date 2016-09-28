70b65a51c9adfe7c27d9a9fc374432a0c91764f3
masukehon <masuke96@gmail.com>
2016-09-28 10:24:09 +0700

    Ahihi

---
+++ List/List/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Element<string> kha = new Element<string>("kha");
+            Element<string> khang = new Element<string>("khang");
+            List<string> list = new List<string>();
+            list.ThemDau(kha);
+            list.ThemDau(khang);
+            list.In();
+        }
+    }
+}
